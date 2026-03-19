76d14de06f19d115bf36683629d240b58c8cb31c
TuanAnhk4 <nguyentuananh2001gl@gmail.com>
2026-03-19 10:19:03 +0700

    chore: initial commit

---
+++ src/main/java/com/example/NguyenTuanAnh/NguyenTuanAnhApplication.java
@@ -0,0 +1,13 @@
+package com.example.NguyenTuanAnh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenTuanAnhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NguyenTuanAnhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NguyenTuanAnh/NguyenTuanAnhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NguyenTuanAnh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NguyenTuanAnhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
